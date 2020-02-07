ff846346e35e2a43dd9a26091baf1bb16febaf4f
ryanmchang <ryanmchang@gmail.com>
2020-02-06 17:54:55 -0800

    Added new screen schedule setup 2

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './screens/Home/Home.js';
-import ScheduleSetup from './screens/ScheduleSetup/ScheduleSetup.js';
+import ScheduleSetup1 from './screens/ScheduleSetup1/ScheduleSetup1.js';
+import ScheduleSetup2 from './screens/ScheduleSetup2/ScheduleSetup2.js';
 
 const NewRoute = () => {
   return (
@@ -21,7 +22,8 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/schedule-setup' component={ScheduleSetup} />
+          <Route exact path='/schedule-setup1' component={ScheduleSetup1} />
+          <Route exact path='/schedule-setup2' component={ScheduleSetup2} />
         </Switch>
       </BrowserRouter>
     );
