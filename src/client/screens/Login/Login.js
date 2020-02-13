de3ce016a9127047435fc7ceff3ec5b0f022eaeb
ashabacon <ashabacon@gmail.com>
2020-02-12 21:31:59 -0800

    no longer perrrty but can login thru Facebook

--- src/client/screens/Login/Login.js
+++ src/client/screens/Login/Login.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
 //import { Route, Link} from 'react-router-dom';
 
 export default class Login extends Component {
-
+  responseFacebook(response) {
+    console.log(response);
+  }
   componentDidMount() {
   }
 
@@ -17,9 +21,16 @@ export default class Login extends Component {
         <p>Welcome to BodyClock</p>
         <h4>Username<input class="textbox" type="text"/></h4>
         <h4>Password<input class="textbox" type="text"/></h4>
-        <a href="account-recovery" id="forgot">Trouble logging in?</a> <br/>
-        <p>or</p>
+        <a href="account-recovery" id="forgot">Trouble logging in?</a><br/>
         <button onClick={this.routeHome}>Login</button>
+        <p>or</p>
+        <FacebookLogin
+          class="facebookLogin"
+          appId="219745835855775"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={this.responseFacebook}
+        /><br/>
       </div>
     );
   }
