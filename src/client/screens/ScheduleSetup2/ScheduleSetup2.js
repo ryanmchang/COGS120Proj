96f14c4febae48466b7cb1cec64d6bc1cc4ab55f
ashabacon <ashabacon@gmail.com>
2020-02-20 12:17:12 -0800

    info icon added

--- src/client/screens/ScheduleSetup2/ScheduleSetup2.js
+++ src/client/screens/ScheduleSetup2/ScheduleSetup2.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import BackButton from "../../components/BackButton/BackButton.js";
 import Agreement from  "../../../../public/agreement.svg";
+import Info from  "../../../../public/info.svg";
 
 export default class ScheduleSetup2 extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showInfo: false,
+    }
+
+    this.showInfo = this.showInfo.bind(this);
+  }
+
+  showInfo(event) {
+    event.preventDefault();
+
+    this.setState({
+      showInfo: true,
+    });
+  }
 
   componentDidMount() {
   }
@@ -19,7 +37,24 @@ export default class ScheduleSetup2 extends Component {
       <div className="setup2">
         <button className="back-button" onClick={this.routeBack}>Back</button>
 
-        <form>What's your Chronotype?<br/>
+        <form>What's your Chronotype?
+        <img className="info" id="infoButton" src={Info} onClick={this.showInfo}
+        align="right"/><br/>
+        {
+          this.state.showInfo
+            ? (
+              <div className="info">
+                <p className="dropdown">A person's chronotype is the propensity for the individual
+                to sleep at a particular time during a 24-hour period. <br/><br/>
+                Don't know what your chronotype is?</p>
+                <a href="https://thepowerofwhenquiz.com/" id="info" >Click this Link</a>
+                <br/><br/>
+              </div>
+            )
+            : (
+              null
+            )
+        }
           <input  type="radio" name="gender" /> Dolphin<br/>
           <input type="radio" name="gender" /> Lion<br/>
           <input type="radio" name="gender" /> Bear<br/>
