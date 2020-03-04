2f7728566e39a4635a84d70a32fc4ae242f31868
ryanmchang <ryanmchang@gmail.com>
2020-03-03 22:03:58 -0800

    Nice

--- src/server/index.js
+++ src/server/index.js
@@ -55,9 +55,9 @@ function writeDrug(value) {
   }});
 }
 
-app.use(express.static(path.join(__dirname + '/../../public')));
+app.use(express.static(path.join(__dirname + '/../../dist')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../public/index.html'));
+  res.sendFile(path.join(__dirname + '/../../dist/index.html'));
 });
 
 console.log(path.join(__dirname + '/../../public/index.html'));
